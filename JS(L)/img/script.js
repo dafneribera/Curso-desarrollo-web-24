c8ac9760828895ef07587fef3fc64005d99cf092
dafneribera <dafneriberaalonso14@gmail.com>
2024-05-17 13:17:46 +0200

    js

--- JS(L)/img/script.js
+++ JS(L)/img/script.js
@@ -4,3 +4,18 @@
 SDJKCNSDDDBCSD*/
 
 console.log("Hola");
+
+let nombre = "Dafne";
+let edad= 22;
+
+console.log(nombre);
+
+console.log("Soy " + nombre + " y tengo " + 22);
+
+console.log(3+8);
+console.log(edad+5);
+console.log(6-19);
+console.log(edad-15);
+
+console.log(4*8);
+console.log(4*8);
