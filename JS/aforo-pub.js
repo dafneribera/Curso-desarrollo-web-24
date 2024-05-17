00f0975408da4e86ca705896a45214d37a8132c3
dafneribera <dafneriberaalonso14@gmail.com>
2024-05-17 10:37:16 +0200

    js

--- JS/aforo-pub.js
+++ JS/aforo-pub.js
@@ -8,5 +8,20 @@ contador = 0;
 
 //Entra un cliente
 contador++;
-
 console.log("Número de personas: " + contador);
+
+
+//Primer caso: Sale un cliente
+console.log('Sale un cliente');
+contador--;
+console.log("Número de personas: " + contador);
+
+//Segundo caso: Llega un cliente
+console.log('Sale un cliente');
+
+if (contador = 20)  {
+//Si se cumple la condición se ejecuta:
+
+} else {
+//Si no se cumple la condición se ejecuta este:
+}
