a63b2ca97b02caf42d9f1816cfaf2e098bf80f27
dafneribera <dafneriberaalonso14@gmail.com>
2024-05-17 10:14:19 +0200

    js

--- JS/aforo-pub.js
+++ JS/aforo-pub.js
@@ -1,12 +1,14 @@
 // Introducción a JavaScript
-console.log("Hola Mundo!");
-console.log(2+1);
+// console.log("Hola Mundo!");
+// console.log(2+1);
 
+
+//INICIO
+console.log('Contador a cero')
 contador = 0;
 
 //Entra un cliente
 contador++;
 contador;
 
-console.log('contador');
-console.log(contador);
+console.log("Número de personas: " + contador);
